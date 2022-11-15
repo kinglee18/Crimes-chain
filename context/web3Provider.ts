ce9500eb965801339140904816bf22673429b40d
king lee <c-king.lee@hulu.com>
2022-11-15 10:15:27 -0600

    Updated: provider props

--- context/web3Provider.ts
+++ context/web3Provider.ts
@@ -3,8 +3,8 @@ import React, { Dispatch } from "react";
 import { MissingPeople } from "../dapp/typechain-types";
 
 interface AppContextInterface {
-    w3Provider:  ethers.providers.Web3Provider | undefined;
-    peopleContract: MissingPeople,
+    w3Provider:  ethers.providers.Web3Provider | ethers.Signer;
+    peopleContract: MissingPeople | undefined,
     setPeopleContract: Dispatch<MissingPeople>
   }
 
